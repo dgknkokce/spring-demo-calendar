e286007ce44d54571cf91265d3325d57fa58ff6c
dogukan.kokce <dogukan.kokce@dbglobe.com>
2025-11-28 12:21:37 +0300

    done crud operations with in memory repository(milestone before switching db)

--- src/main/java/com/dodo/demo_calendar/controller/ContentController.java
+++ src/main/java/com/dodo/demo_calendar/controller/ContentController.java
@@ -3,10 +3,7 @@ package com.dodo.demo_calendar.controller;
 import com.dodo.demo_calendar.model.Content;
 import com.dodo.demo_calendar.repository.ContentCollectionRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -30,6 +27,26 @@ public class ContentController {
     @GetMapping("/{id}")
     public Content findById(@PathVariable Integer id){
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Content is not found!"));
+    }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content){
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id){
+        if(!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Content is not found!");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        repository.delete(id);
     }
 }
