b0b9bd6c3c5e15ae51e2bfbedf67853579da11a8
dogukan.kokce <dogukan.kokce@dbglobe.com>
2025-11-28 21:36:17 +0300

    add h2 database and it's dependencies

--- src/main/java/com/dodo/demo_calendar/controller/ContentController.java
+++ src/main/java/com/dodo/demo_calendar/controller/ContentController.java
@@ -2,6 +2,7 @@ package com.dodo.demo_calendar.controller;
 
 import com.dodo.demo_calendar.model.Content;
 import com.dodo.demo_calendar.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,7 +33,7 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content){
+    public void create(@Valid @RequestBody Content content){
         repository.save(content);
     }
 
